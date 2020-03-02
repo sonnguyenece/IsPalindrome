df8862e6c3c221e10d92c19e32b1c9814c0f97af
sonnguyenece <sonnguyenece@gmail.com>
2020-03-02 18:37:24 +0700

    palindrome by recursive function

---
+++ palindrome.js
@@ -0,0 +1,7 @@
+function isPalindrome(string) {
+    if (string.length <= 1) return true;
+    if (string[0] !== string[string.length - 1]) return false;
+    string = string.substring(1, string.length - 1);
+    if (isPalindrome(string)) return true;
+    else return false;
+}
