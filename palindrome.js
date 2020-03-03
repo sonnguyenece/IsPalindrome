d7cd604a7ea115ac10d15c4ad8444015ffaefb64
sonnguyenece <sonnguyenece@gmail.com>
2020-03-03 08:14:48 +0700

    update

--- palindrome.js
+++ palindrome.js
@@ -2,6 +2,5 @@ function isPalindrome(string) {
     if (string.length <= 1) return true;
     if (string[0] !== string[string.length - 1]) return false;
     string = string.substring(1, string.length - 1);
-    if (isPalindrome(string)) return true;
-    else return false;
+    return isPalindrome(string) ;
 }
